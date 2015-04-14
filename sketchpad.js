bdf1e7a90bcec2d6512f6865d466c0dd63e4efcf
Sean <sgroff04@gmail.com>
2015-04-14 13:16:11 -0500

    Grid Design 0.1

---
+++ sketchpad.js
@@ -0,0 +1,15 @@
+$(function() {
+	var $grid = $('#container');
+
+	for (i = 0; i < 16; i++) 
+	{
+		var row = '<div>';
+    
+    	for (j = 0; j < 16; j++)
+        	row += '<div class="square">' + j + '</div>';
+    
+    	row += '</div>';
+    
+    	$grid.append(row);
+	}
+});
